18ba99a95f046788d62a7afc389b437e5d95f787
moises-herrera <moisesherrera037@gmail.com>
2025-03-23 10:14:35 -0500

    refactor: update migration

--- src/migrations/Migration20250323150931.ts
+++ src/migrations/Migration20250323150931.ts
@@ -1,20 +1,19 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20250323033138 extends Migration {
+export class Migration20250323150931 extends Migration {
 
   override async up(): Promise<void> {
     this.addSql(`create table "permission" ("id" uuid not null, "created_at" timestamptz not null, "updated_at" timestamptz not null, "name" varchar(100) not null, "description" varchar(200) null, constraint "permission_pkey" primary key ("id"));`);
     this.addSql(`alter table "permission" add constraint "permission_name_unique" unique ("name");`);
 
-    this.addSql(`create table "user" ("id" uuid not null, "created_at" timestamptz not null, "updated_at" timestamptz not null, "first_name" varchar(20) not null, "last_name" varchar(20) not null, "username" varchar(20) not null, "email" varchar(50) not null, "password" text not null, constraint "user_pkey" primary key ("id"));`);
-    this.addSql(`alter table "user" add constraint "user_username_unique" unique ("username");`);
+    this.addSql(`create table "user" ("id" uuid not null, "created_at" timestamptz not null, "updated_at" timestamptz not null, "first_name" varchar(20) not null, "last_name" varchar(20) not null, "email" varchar(50) not null, "password" text not null, constraint "user_pkey" primary key ("id"));`);
     this.addSql(`alter table "user" add constraint "user_email_unique" unique ("email");`);
 
     this.addSql(`create table "folder" ("id" uuid not null, "created_at" timestamptz not null, "updated_at" timestamptz not null, "name" varchar(100) not null, "owner_id" uuid not null, "parent_folder_id" uuid null, constraint "folder_pkey" primary key ("id"));`);
 
     this.addSql(`create table "folder_permission" ("id" uuid not null, "created_at" timestamptz not null, "updated_at" timestamptz not null, "folder_id" uuid not null, "user_id" uuid not null, "permission_id" uuid not null, constraint "folder_permission_pkey" primary key ("id"));`);
 
-    this.addSql(`create table "file" ("id" uuid not null, "created_at" timestamptz not null, "updated_at" timestamptz not null, "name" varchar(100) not null, "folder_id" uuid not null, constraint "file_pkey" primary key ("id"));`);
+    this.addSql(`create table "file" ("id" uuid not null, "created_at" timestamptz not null, "updated_at" timestamptz not null, "name" varchar(100) not null, "extension" varchar(100) not null, "size" int not null, "mime_type" varchar(100) not null, "storage_path" varchar(255) not null, "owner_id" uuid not null, "folder_id" uuid not null, constraint "file_pkey" primary key ("id"));`);
 
     this.addSql(`create table "file_permission" ("id" uuid not null, "created_at" timestamptz not null, "updated_at" timestamptz not null, "file_id" uuid not null, "user_id" uuid not null, "permission_id" uuid not null, constraint "file_permission_pkey" primary key ("id"));`);
 
@@ -25,6 +24,7 @@ export class Migration20250323033138 extends Migration {
     this.addSql(`alter table "folder_permission" add constraint "folder_permission_user_id_foreign" foreign key ("user_id") references "user" ("id") on update cascade;`);
     this.addSql(`alter table "folder_permission" add constraint "folder_permission_permission_id_foreign" foreign key ("permission_id") references "permission" ("id") on update cascade;`);
 
+    this.addSql(`alter table "file" add constraint "file_owner_id_foreign" foreign key ("owner_id") references "user" ("id") on update cascade;`);
     this.addSql(`alter table "file" add constraint "file_folder_id_foreign" foreign key ("folder_id") references "folder" ("id") on update cascade;`);
 
     this.addSql(`alter table "file_permission" add constraint "file_permission_file_id_foreign" foreign key ("file_id") references "file" ("id") on update cascade;`);
@@ -40,6 +40,8 @@ export class Migration20250323033138 extends Migration {
     this.addSql(`alter table "folder" drop constraint "folder_owner_id_foreign";`);
 
     this.addSql(`alter table "folder_permission" drop constraint "folder_permission_user_id_foreign";`);
+
+    this.addSql(`alter table "file" drop constraint "file_owner_id_foreign";`);
 
     this.addSql(`alter table "file_permission" drop constraint "file_permission_user_id_foreign";`);
 
