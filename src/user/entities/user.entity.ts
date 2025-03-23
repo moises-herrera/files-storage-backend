65dc9d2551afeacf7b264d3f59b398c2ba267ef9
moises-herrera <moisesherrera037@gmail.com>
2025-03-23 10:14:25 -0500

    refactor: update user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -10,9 +10,10 @@ import {
 } from '@mikro-orm/core';
 import { hash, verify } from 'argon2';
 import { BaseEntity } from 'src/common/entities/base.entity';
+import { File } from 'src/storage/entities/file.entity';
+import { Folder } from 'src/storage/entities/folder.entity';
 import { FilePermission } from 'src/storage/entities/file-permission.entity';
 import { FolderPermission } from 'src/storage/entities/folder-permission.entity';
-import { Folder } from 'src/storage/entities/folder.entity';
 
 @Entity()
 export class User extends BaseEntity {
@@ -22,35 +23,33 @@ export class User extends BaseEntity {
   @Property({ type: t.string, length: 20 })
   lastName: string;
 
-  @Property({ type: t.string, length: 20, unique: true })
-  username: string;
-
   @Property({ type: t.string, length: 50, unique: true })
   email: string;
 
   @Property({ type: t.text, hidden: true, lazy: true })
   password: string;
 
+  @OneToMany({ entity: () => File, mappedBy: 'owner' })
+  files = new Collection<File>(this);
+
   @OneToMany({ entity: () => Folder, mappedBy: 'owner' })
   folders = new Collection<Folder>(this);
-
-  @OneToMany({ entity: () => FolderPermission, mappedBy: 'user' })
-  folderPermissions = new Collection<FolderPermission>(this);
 
   @OneToMany({ entity: () => FilePermission, mappedBy: 'user' })
   filePermissions = new Collection<FilePermission>(this);
 
+  @OneToMany({ entity: () => FolderPermission, mappedBy: 'user' })
+  folderPermissions = new Collection<FolderPermission>(this);
+
   constructor(
     firstName: string,
     lastName: string,
-    username: string,
     email: string,
     password: string,
   ) {
     super();
     this.firstName = firstName;
     this.lastName = lastName;
-    this.username = username;
     this.email = email;
     this.password = password;
   }
