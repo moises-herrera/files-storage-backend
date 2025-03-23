a0a8c1845755ebe9732dfa64658dc24064224e4a
moises-herrera <moisesherrera037@gmail.com>
2025-03-23 10:14:15 -0500

    refactor: add file columns

--- src/storage/entities/file.entity.ts
+++ src/storage/entities/file.entity.ts
@@ -9,11 +9,27 @@ import {
 import { BaseEntity } from 'src/common/entities/base.entity';
 import { Folder } from './folder.entity';
 import { FilePermission } from './file-permission.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Entity()
 export class File extends BaseEntity {
   @Property({ type: t.string, length: 100 })
   name: string;
+
+  @Property({ type: t.string, length: 100 })
+  extension: string;
+
+  @Property({ type: t.integer })
+  size: number;
+
+  @Property({ type: t.string, length: 100 })
+  mimeType: string;
+
+  @Property({ type: t.string })
+  storagePath: string;
+
+  @ManyToOne({ entity: () => User })
+  owner: User;
 
   @ManyToOne({ entity: () => Folder })
   folder: Folder;
