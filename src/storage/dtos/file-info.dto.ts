c0b178d9210d5862542c6272f68ed7c93d9361a9
moises-herrera <moisesherrera037@gmail.com>
2025-03-27 10:21:35 -0500

    chore: add file info DTO

---
+++ src/storage/dtos/file-info.dto.ts
@@ -0,0 +1,19 @@
+export class FileInfoDto {
+  readonly id: string;
+  readonly name: string;
+  readonly extension: string;
+  readonly size: number;
+  readonly mimeType: string;
+  readonly owner: {
+    readonly id: string;
+    readonly firstName: string;
+    readonly lastName: string;
+    readonly email: string;
+  };
+  readonly folder: {
+    readonly id: string;
+    readonly name: string;
+  };
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+}
