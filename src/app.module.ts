1450a595d13c5bb2dd2977e931416f98708be073
moises-herrera <moisesherrera037@gmail.com>
2025-03-28 17:31:55 -0500

    chore: setup serve static module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
-import { Module, OnModuleInit } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import config from './config/mikro-orm.config';
 import { UserModule } from './user/user.module';
 import { StorageModule } from './storage/storage.module';
-import { MikroORM } from '@mikro-orm/postgresql';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(config), UserModule, StorageModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MikroOrmModule.forRoot(config),
+    UserModule,
+    StorageModule,
+  ],
 })
-export class AppModule implements OnModuleInit {
-  constructor(private readonly orm: MikroORM) {}
-
-  async onModuleInit(): Promise<void> {
-    await this.orm.getMigrator().up();
-  }
-}
+export class AppModule {}
