197056c9b57dd8983ab5d0bac51cd023911aa04b
moises-herrera <moisesherrera037@gmail.com>
2025-06-15 20:45:08 -0500

    chore: add frontend files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,15 @@ import { MikroORM, PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { AppConfigModule } from './config/app-config.module';
 import { ConfigService } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'node:path';
+import { EnvConfig } from './config/env.config';
 
 @Module({
   imports: [
     AppConfigModule,
     MikroOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => {
+      useFactory: (configService: ConfigService<EnvConfig, true>) => {
         return {
           ...config,
           host: configService.get<string>('DB_HOST'),
@@ -24,6 +27,9 @@ import { HealthModule } from './health/health.module';
       },
       inject: [ConfigService],
       driver: PostgreSqlDriver,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     HealthModule,
     UserModule,
